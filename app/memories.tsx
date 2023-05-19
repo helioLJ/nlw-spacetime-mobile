590ec2e495341a3d7c23cb6a08dadf22dce410da
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-19 19:16:20 -0300

    memory page

--- app/memories.tsx
+++ app/memories.tsx
@@ -1,9 +1,78 @@
-import { View, Text } from 'react-native'
+import { ScrollView, View, TouchableOpacity, Text, Image } from 'react-native'
+import Icon from '@expo/vector-icons/Feather'
 
-export default function Memories() {
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { Link, useRouter } from 'expo-router'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as SecureStore from 'expo-secure-store'
+
+export default function NewMemory() {
+  const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
+  async function signOut() {
+    await SecureStore.deleteItemAsync('token')
+
+    router.push('/')
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Memories</Text>
-    </View>
+    <ScrollView
+      className="flex-1 px-8"
+      contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
+    >
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo />
+
+        <View className="flex-row gap-2">
+          <TouchableOpacity
+            onPress={signOut}
+            className="h-10 w-10 items-center justify-center rounded-full bg-red-500"
+          >
+            <Icon name="log-out" size={16} color="#fff" />
+          </TouchableOpacity>
+
+          <Link href="/new" asChild>
+            <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-green-500">
+              <Icon name="plus" size={16} color="#fff" />
+            </TouchableOpacity>
+          </Link>
+        </View>
+      </View>
+
+      <View className="mt-6 space-y-10">
+        <View className="space-y-4">
+          <View className="flex-row items-center gap-2">
+            <View className="h-px w-5 bg-gray-50"></View>
+            <Text className="font-body text-xs text-gray-100">
+              12 de Abril, 2023
+            </Text>
+          </View>
+          <View className="space-y-4">
+            <Image
+              source={{
+                uri: 'http://192.168.1.22:3333/uploads/677e8913-16d3-43db-a9f1-a90367a63431.jpg',
+              }}
+              className="aspect-video w-full rounded-lg"
+              alt=""
+            />
+            <Text className="font-body text-base leading-relaxed text-gray-100">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Consequatur sapiente cumque cupiditate, temporibus odit esse at
+              animi ipsum aliquam maiores culpa! Explicabo, distinctio maiores
+              autem odit illo veritatis. Voluptatum, totam.
+            </Text>
+            <Link href="/memories/id" asChild>
+              <TouchableOpacity className="flex-row items-center gap-2">
+                <Text className="font-body text-sm text-gray-200">
+                  Ler Mais
+                </Text>
+                <Icon name="arrow-right" size={16} color="#9e9ea0" />
+              </TouchableOpacity>
+            </Link>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   )
 }
