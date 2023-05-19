2d85cf7b39e1f020c7220733a4feb15695c5e089
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-19 15:38:32 -0300

    new memory page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,18 +13,32 @@ import Stripes from '../src/assets/stripes.svg'
 import { ImageBackground } from 'react-native'
 import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import * as SecureStore from 'expo-secure-store'
+import { useEffect, useState } from 'react'
 
 const StyledStripes = styled(Stripes)
 
 export default function Layout() {
+  const [isUserAuthenticated, setIsUserAuthenticated] = useState<
+    null | boolean
+  >(null)
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
+
+  useEffect(() => {
+    SecureStore.getItemAsync('token').then((token) => {
+      setIsUserAuthenticated(!!token)
+    })
+  }, [])
+
   if (!hasLoadedFonts) {
     return <SplashScreen />
   }
+
   return (
     <ImageBackground
       source={blurBg}
@@ -39,7 +53,11 @@ export default function Layout() {
           headerShown: false,
           contentStyle: { backgroundColor: 'transparent' },
         }}
-      />
+      >
+        <Stack.Screen name="index" redirect={isUserAuthenticated} />
+        <Stack.Screen name="new" />
+        <Stack.Screen name="memories" />
+      </Stack>
     </ImageBackground>
   )
 }
