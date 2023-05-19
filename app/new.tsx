f46f0d2835bb6e99f89e8d9561ed8034d16acd72
Hélio Lúcio <helio.lucio.jr@hotmail.com>
2023-05-19 18:57:10 -0300

    criando nova memoria

--- app/new.tsx
+++ app/new.tsx
@@ -10,13 +10,16 @@ import {
 import Icon from '@expo/vector-icons/Feather'
 
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
 import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
+import { api } from '../src/lib/api'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
   const [preview, setPreview] = useState<string | null>(null)
   const [content, setContent] = useState('')
@@ -35,8 +38,44 @@ export default function NewMemory() {
     } catch (error) {}
   }
 
-  function handleCreateMemory() {
-    console.log(content, isPublic)
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+
+      await api.post(
+        '/memories',
+        {
+          content,
+          isPublic,
+          coverUrl,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      )
+    }
+
+    router.push('/memories')
   }
 
   return (
